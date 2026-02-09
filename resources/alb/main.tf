d3532467ed731e6ae235f6bb91d48cf11968047c
galactus-clb <chanakabalasuriya123@gmail.com>
2026-02-09 19:11:00 +0530

    Added health check

--- resources/alb/main.tf
+++ resources/alb/main.tf
@@ -50,11 +50,21 @@ resource "aws_lb_target_group" "this" {
   port = 80
   protocol = "HTTP"
   vpc_id = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "register_ec2" {
   target_group_arn = aws_lb_target_group.this.arn
   target_id = var.ec2_id
+  port = 80
 }
 
 resource "aws_lb_listener" "this" {
